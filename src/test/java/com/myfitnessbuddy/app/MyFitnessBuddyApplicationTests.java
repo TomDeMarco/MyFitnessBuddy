8ba968a283fe3d7773eb741afa62545436342f4c
ThomasNoto <tnoto82@gmail.com>
2025-03-21 17:34:41 -0400

    configured H2 database to be persistent (see data folder in root directory)

--- src/test/java/com/myfitnessbuddy/app/MyFitnessBuddyApplicationTests.java
+++ src/test/java/com/myfitnessbuddy/app/MyFitnessBuddyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyFitnessBuddyApplicationTests {
+public class MyFitnessBuddyApplicationTests {
 
 	@Test
 	void contextLoads() {
